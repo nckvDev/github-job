704baf4b952cd8e59143495e13dd335ae20909bf
nckvDev <jirapong.ku@rmuti.ac.th>
2022-03-07 17:20:25 +0700

    cancle Job: 403!

--- job-app/src/App.js
+++ job-app/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import useFetchJobs from "./useFetchjobs";
+import { Container } from "react-bootstrap";
+import { useState } from "react";
+import Job from "./Job";
 
 function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error} = useFetchJobs()
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refeashing...</h1>}
+      <h1>{jobs.length}</h1>
+      {jobs.map(job => {
+        return <Job key={job?.id} job={job} />
+      })}
+    </Container>
   );
 }
 
---
+++ job-app/src/Job.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Job({ job }) {
+  return (
+    <div>
+        {job}
+    </div>
+  )
+}
+
+export default Job
